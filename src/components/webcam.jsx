ea95ab5ca76df0d6f680a3b2c2e519b44552ae3a
leon <leonnupa8@gmail.com>
2026-06-05 18:27:46 +0100

    Detect hand gestures

--- src/components/webcam.jsx
+++ src/components/webcam.jsx
@@ -2,23 +2,31 @@ import './webcam.css';
 import Webcam from 'react-webcam';
 import { DrawingUtils, HandLandmarker, FilesetResolver, GestureRecognizer } from '@mediapipe/tasks-vision';
 import { useEffect, useRef } from 'react';
-import { useGestureDetection } from '../hooks/useGestureDetection.js';
+import  useGestureDetection  from '../hooks/useGestureDetection.js';
+
 
 function WebcamCanvas() {
     //Call the custom hook to set up gesture detection and drawing on the canvas.
-    const webcameRef = useRef(null);
+    const webcamRef = useRef(null);
     const canvasRef = useRef(null);
-    const gestureRecognizer = useRef(null);
-    useGestureDetection(webcameRef, canvasRef, gestureRecognizer);
+  
+
+    const { left, right } = useGestureDetection(webcamRef, canvasRef);
+    console.log("My current gestures are:", left, right);
+
     return (
         <div className="webcam-container">
-            <Webcam ref={webcameRef}
+            <Webcam ref={webcamRef}
                 className="webcam-feed"
                 audio={false}
                 width={400}
                 height={300}
                 videoConstraints={{ facingMode: "user" }}
             />
+            <div>
+                <p>Right hand: {right}</p>
+                <p>left hand: {left}</p>
+            </div>
             {/* The canvas sits directly on top of the webcam feed. */}
             <canvas ref={canvasRef} className="webcam-canvas" />
         </div>
--- src/hooks/useGestureDetection.js
+++ src/hooks/useGestureDetection.js
@@ -1,76 +1,119 @@
- //import dependecies
- import {useEffect, useRef} from 'react';
- import { DrawingUtils, HandLandmarker, FilesetResolver, GestureRecognizer } from '@mediapipe/tasks-vision';
- 
- // This custom hook encapsulates the gesture detection logic, making it reusable across components.
- export function useGestureDetection(webcameRef, canvasRef, gestureRecognizer) {
-    
+        //import dependecies
+        import {useEffect, useRef, useState} from 'react';
+        import { DrawingUtils, HandLandmarker, FilesetResolver, GestureRecognizer } from '@mediapipe/tasks-vision';
+        
+        //hook to move it between componenets 
+        export default function useGestureDetection(webcamRef, canvasRef) {
+            
+            //use ref to not crash , and use state to hold gesture result
+            const gestureRecognizer = useRef(null)
+            const [gesture, setGesture] = useState({left: null, right: null})
 
-   
+            useEffect(() => {
 
-    useEffect(() => {
-        let animationFrameId; // Track the loop ID so we can cancel it on cleanup.
+                
+                let animationFrameId; 
 
-        //setup async function to initialize the AI and start the loop.
-        const setup = async () => {
-            // Initialize the MediaPipe vision task engine (FilesetResolver).
-            const vision = await FilesetResolver.forVisionTasks(
-                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-            );
+                //Identify Gesture function
+                    function identifyGesture(results) {
+                        const newGestures = {
+                            left: null,
+                            right: null
+                        };
 
-            // Create the recognizer with the specified model and options.
-            gestureRecognizer.current = await GestureRecognizer.createFromOptions(vision, {
-                baseOptions: {
-                    modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
-                    delegate: "GPU"
-                },
-                runningMode: "VIDEO"
-            });
+                        for (let i = 0; i < results.gestures.length; i++) {
 
-            //set the cavans size to match the webcam feed.
-            if (canvasRef.current) {
-                canvasRef.current.width = 400;
-                canvasRef.current.height = 300;
-            }
+                            if (results.handedness[i] === undefined) {
+                                continue;
+                            }
 
-            //renderLoop is the recursive function called by the browser's refresh rate.
-            function renderLoop() {
-                const video = webcameRef.current?.video;
-                const canvasCtx = canvasRef.current?.getContext('2d');
+                            const handInfo = results.handedness[i];
+                            const gestureInfo = results.gestures[i];
 
-                // Check if AI is ready AND video is playing
-                if (gestureRecognizer.current && video?.readyState === 4 && canvasCtx) {
-                    // Process the current frame to get landmarks.
-                    const results = gestureRecognizer.current.recognizeForVideo(video, Date.now());
-                    
-                    // Clear previous frame drawing.
+                            if (handInfo.length === 0 || gestureInfo.length === 0) {
+                                continue;
+                            }
+
+                            const hand = handInfo[0].displayName.toLowerCase();
+                            const gesture = gestureInfo[0].categoryName;
+
+                            newGestures[hand] = gesture;
+                            console.log(results);
+                            console.log(results.gestures);
+                            console.log(results.handedness);
+                        }
+
+                        setGesture(newGestures);
+                    }
+                //Drawing Function
+                function drawVisuals(canvasCtx, results){
                     canvasCtx.clearRect(0, 0, 400, 300);
 
-                    // If hands are detected, loop through them.
-                    if (results.landmarks) {
-                        // DrawingUtils MUST be instantiated with the context.
-                        const drawingUtils = new DrawingUtils(canvasCtx);
-                        for (const landmarks of results.landmarks) {
-                            // Draw the skeleton lines.
-                            drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
-                            // Draw the points (nodes).
-                            drawingUtils.drawLandmarks(landmarks, { color: '#FF0000', lineWidth: 2 });
+                                // If hands are detected, loop through them.
+                        if (results.landmarks) {
+                                    // DrawingUtils MUST be instantiated with the context.
+                            const drawingUtils = new DrawingUtils(canvasCtx);
+
+                            for (const landmarks of results.landmarks) {
+                                        // Draw the skeleton lines.
+                                drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 4 });
+                                        // Draw the points (nodes).
+                                drawingUtils.drawLandmarks(landmarks, { color: '#FF0000', lineWidth: 1 });
+                            }
                         }
-                    }
                 }
-                // Schedule the next frame update.
-                animationFrameId = requestAnimationFrame(renderLoop);
-            }
-            
-            // Start the loop after everything is loaded.
-            renderLoop();
-        };
+                
+                //setup async function to initialize the AI and start the loop.
+                const setup = async () => {
 
-        setup();
+                    // Initialise the MediaPipe vision task engine 
+                    const vision = await FilesetResolver.forVisionTasks(
+                        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+                    );
 
-        //stop the loop when the component is removed to prevent memory leaks.
-        return () => cancelAnimationFrame(animationFrameId);
-    }, [webcameRef, canvasRef, gestureRecognizer]);
+                    // Create the recognizer with the specified model and options.
+                    gestureRecognizer.current = await GestureRecognizer.createFromOptions(vision, {
+                        baseOptions: {
+                            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+                            delegate: "GPU"
+                        },
+                        numHands: 2,
+                        runningMode: "VIDEO",
+                        minHandDetectionConfidence: 0.7
+                    });
 
- }
+                    //set the cavans size to match the webcam feed.
+                    if (canvasRef.current) {
+                        canvasRef.current.width = 400;
+                        canvasRef.current.height = 300;
+                    }
 
+                
+                    function renderLoop() {
+                        const video = webcamRef.current?.video;
+                        const canvasCtx = canvasRef.current?.getContext('2d');
+
+                            // Check if AI is ready AND video is playing
+                            if (gestureRecognizer.current && video?.readyState === 4 && canvasCtx) {
+                                // Process the current frame to get landmarks.
+                            const results = gestureRecognizer.current.recognizeForVideo(video, Date.now());
+
+                            drawVisuals(canvasCtx,results);
+                            if(results.gestures.length > 0){
+                                identifyGesture(results)
+                            }
+                        }
+                        animationFrameId = requestAnimationFrame(renderLoop);
+                    }
+                    // Start the loop after everything is loaded.
+                    renderLoop();
+                };
+                setup();
+                //stop the loop when the component is removed to prevent memory leaks.
+                return () => cancelAnimationFrame(animationFrameId);
+
+            }, [webcamRef, canvasRef, gestureRecognizer]);
+
+            //Returning the gesture
+            return gesture;
+        }
